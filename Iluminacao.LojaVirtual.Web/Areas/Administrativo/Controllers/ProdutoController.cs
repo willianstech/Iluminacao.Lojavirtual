46128fee43b00da5fdd5edb06f7786bcfef59e62
willianstech <willian669@hotmail.com>
2015-01-12 18:36:41 -0200

    Area Administrativa

---
+++ Iluminacao.LojaVirtual.Web/Areas/Administrativo/Controllers/ProdutoController.cs
@@ -0,0 +1,33 @@
+﻿using Iluminacao.Lojavirtual.Dominio.Entidade;
+using Iluminacao.Lojavirtual.Dominio.Repositorio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Iluminacao.LojaVirtual.Web.Areas.Administrativo.Controllers
+{
+    public class ProdutoController : Controller
+    {        
+
+        private ProdutosRepositorio _repositorio;
+
+
+        public ActionResult Index()
+        {
+            _repositorio = new ProdutosRepositorio();
+            var produtos = _repositorio.Produtos;
+            return View(produtos);
+        }
+
+        public ViewResult Alterar(int produtoId)
+        {
+            _repositorio = new ProdutosRepositorio();
+            Produto produto = _repositorio.Produtos
+                .FirstOrDefault(p => p.ProdutoId == produtoId);
+
+                return View(produto);
+        }
+    }
+}
--- Iluminacao.Lojavirtual.Dominio/Entidade/Produto.cs
+++ Iluminacao.Lojavirtual.Dominio/Entidade/Produto.cs
@@ -3,13 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+
 
 namespace Iluminacao.Lojavirtual.Dominio.Entidade
 {
     public class Produto
     {
+        [HiddenInput(DisplayValue = false)]
         public int ProdutoId { get; set; }
         public string Nome { get; set; }
+
+        [DataType(DataType.MultilineText)]
         public string Descricao { get; set; }
         public decimal Preco { get; set; }
         public string Categoria { get; set; }
